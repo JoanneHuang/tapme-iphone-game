6ecd9a9e1241e883d7d541996112924b21a1aa91
JoanneHuang <you@example.com>
2014-07-22 19:24:47 -0700

    beginning the project

---
+++ Tap Me/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by Joanne Huang on 7/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+    IBOutlet UILabel *scoreLabel;
+    IBOutlet UILabel *timerLabel;
+}
+
+- (IBAction)buttonPressed;
+
+@end
---
+++ Tap Me/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Tap Me
+//
+//  Created by Joanne Huang on 7/21/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) buttonPressed{
+    scoreLabel.text = @"Pressed!";
+}
+@end
